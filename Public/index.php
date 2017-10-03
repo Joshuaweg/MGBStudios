fa6f2a8459e4b25c8b0edbdceb22717a169fe9da
joshuaweg <joshua.weg.95@gmail.com>
2017-10-02 21:26:15 -0700

    this is not final

--- Public/index.php
+++ Public/index.php
@@ -4,11 +4,11 @@
 <head>
   <meta charset="utf-8">
 
-  <title>index.php</title>
+  <title>MGB Studios</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="author" content="SitePoint">
 
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <link rel="stylesheet" href="stylesheets">
 
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
@@ -16,12 +16,20 @@
 </head>
 
 <body>
+	<header>
+	<h1> MGB Studios </h1>
+	</header>
+	<nav>
 
+	</nav>
+	<footer>
+
+	</footer>
 <?PHP 
 
 //First index main page
 
-
+echo "hello";
 
 
 
